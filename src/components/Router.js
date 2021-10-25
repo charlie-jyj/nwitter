dad2f6c171e7117db591a8b939c4aa3f3db29cb5
jinietree <jinietree20@gmail.com>
2021-10-26 00:25:31 +0900

    after router

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
 
 const AppRouter = () => {
+  // [상태, 상태 관리 함수] js destructuring assignment
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
   return (
     <Router>
       {/* switch: 여러 Route  중 하나만 렌더링 할 수 있게 해준다. */}
       <Switch>
-        <Route></Route>
+        {isLoggedIn? 
+          (
+            <Route exact path="/">
+              <Home />
+            </Route>
+          ) : (
+            <Route exact path="/">
+              <Auth />
+            </Route>
+          )}
       </Switch>  
     </Router>
       
   )
 }
+
+export default AppRouter;
